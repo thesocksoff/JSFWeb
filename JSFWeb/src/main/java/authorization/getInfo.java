6ac3430e495ad3ecca181d465e016efd7d75fd36
Константин <thesocksoff@gmail.com>
2018-04-18 15:37:21 +0300

    Add files via upload
    эксперементальная ветка

--- JSFWeb/src/main/java/authorization/getInfo.java
+++ JSFWeb/src/main/java/authorization/getInfo.java
@@ -1,16 +1,50 @@
 package authorization;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.io.Serializable;
 
 @ManagedBean
-public class getInfo {
+public class getInfo implements Serializable{
+	private String login;
 	private String password;
+	//private String validate;
+
+	private static final long serialVersionUID = 1L;
 	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
 	public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword1(String password) {
-    	this.password = password;
-    }
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getValidate() {
+		return "SUCSESS";
+	}
+	
+	public String getIndex() {
+		return "index.xhtml";
+	}
+	
+	public void loginAction(ActionEvent actionEvent) {
+		loginMessage("Welcome to index!!");
+	}
+
+	public void loginMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+
+	}
 }
---
+++ JSFWeb/src/main/resources/db/migration/V3_1__Add_User_table.sql
@@ -0,0 +1,5 @@
+create table users (
+	id int primary key not null,
+    name varchar(100) not null,
+    password varchar(100) not null
+);
